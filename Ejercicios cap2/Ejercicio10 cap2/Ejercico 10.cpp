42787ac8ad535b93384e0846d5939c1ac3bb4208
Leonard Pacheco <133384126+20198589@users.noreply.github.com>
2023-06-01 17:07:20 -0400

    Add files via upload
    EJERCICIOS CAP 2 Y 3

---
+++ Ejercicios cap2/Ejercicio10 cap2/Ejercico 10.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main(void) {
+  int NUM;
+
+  printf("Introduce el número: ");
+  scanf("%d", &NUM);
+
+  if (NUM == 0) {
+    printf("\nNulo");
+  } else {
+    if (NUM % 2 == 0) {
+      printf("\nPar");
+    } else {
+      printf("\nImpar");
+    }
+  }
+}
